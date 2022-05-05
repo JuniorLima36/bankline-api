10fd2ab5b32b1de4dd6968d762cfe45ef4a1dfc3
JuniorLima36 <104943798+JuniorLima36@users.noreply.github.com>
2022-05-05 14:54:27 -0300

    Entidades mapeadas

---
+++ src/main/java/com/dio/santander/bankline/api/model/Conta.java
@@ -0,0 +1,26 @@
+package com.dio.santander.bankline.api.model;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Conta {
+	@Column(name = "conta_numero")
+	private long numero;
+	
+	@Column(name = "conta_saldo")
+	private double saldo;
+	
+	public long getNumero() {
+		return numero;
+	}
+	public void setNumero(long numero) {
+		this.numero = numero;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+}
